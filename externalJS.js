eda31659111b406936b67f0856512c16e57f39c1
SANJAY KUMAR BR <snjkumarbr@gmail.com>
2021-06-04 23:11:47 +0530

    commit 4.6

---
+++ externalJS.js
@@ -0,0 +1,22 @@
+console.log('Welcome to JS'); //print in console
+document.write("WEll");   //print in  web page
+var nameo = "datatype in js";
+console.log(nameo);
+
+window.alert("This is a JS File");
+//this creates a pop up window
+var confirmation =window.confirm("Are you sure to delete");
+//you get ok or cancel
+
+console.log(confirmation);
+
+var input =window.prompt("Enter your name")
+console.log(input);
+
+window.print();
+
+//window.open();
+//window.close();
+
+
+//commenting in java /* */
